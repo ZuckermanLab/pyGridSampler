a0b4e58af5491db6b501520c39866a032d521366
August <30163079+augeorge@users.noreply.github.com>
2023-04-04 03:57:38 -0700

    docs

--- pyGridSampler/__init__.py
+++ pyGridSampler/__init__.py
@@ -1,3 +1,12 @@
 r'''
 PyGridSampler is a package for adaptive grid sampling using data-tempering
+
+example usage:
+```
+import GridSampler as gs
+
+sampler = gs.GridSampler(log_prob_func, func_args, data, x_bounds, x_extension)
+results = sampler.initialize_and_sample(init_grid_resolution, init_data_size, ess_min, delta, n_processes=4, max_iter=100)
+```
+
 '''
